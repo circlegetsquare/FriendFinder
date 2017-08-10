7aebb4538429c8a2f290eb56620e8cbe01234822
Joshua Miller <jlmiller07@gmail.com>
2017-08-10 00:41:58 -0500

    added comparison logic. app functioning.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,14 +10,36 @@ module.exports = function(app) {
     res.json(arrFriends);
   });
 
-  //adds incoming survey data
-  app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
-    console.log(arrFriends);
-    newFriend.scores = newFriend.scores.map(Number);
-    
-    console.log(newFriend);
+    //adds incoming survey data
+    app.post("/api/friends", function(req, res) {
+      var newFriend = req.body;
+      //changes array of string numbers to values
+      newFriend.scores = newFriend.scores.map(Number);
+      
+      //calculates absolute differences between friends scores and returns postition of minimum difference
+      function calcBestMatch(){
+        var arrDiffs = [];
+        for (var i = 0; i < arrFriends.length; i++) {
+          var friendDiff = 0;
+          for (var j = 0; j < arrFriends[i].scores.length; j++) {
+            var scoreDiff = arrFriends[i].scores[j]-newFriend.scores[j];
+            friendDiff += Math.abs(scoreDiff);
+          };
+          arrDiffs.push(friendDiff);
+          //console.log(arrDiffs);
+          //console.log("Min array val" + Math.min(...arrDiffs));
+        };  
+
+    var minIndex = arrDiffs.indexOf(Math.min(...arrDiffs));
+    //console.log("min Index" + minIndex);
+
+    //adds new friend to friend array
     arrFriends.push(newFriend);
-    res.json(newFriend);   
+    //console.log(arrFriends);
+
+    //returns position of friend with minimum difference
+    return res.json(arrFriends[minIndex]);
+    };
+  calcBestMatch();
   });
-};
+}
