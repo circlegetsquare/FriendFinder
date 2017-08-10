eceec614ccb755a305de4f512c370301e5241346
Joshua Miller <jlmiller07@gmail.com>
2017-08-09 20:38:33 -0500

    updating routing, adding html and new questions

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,21 @@
 // API Routes
 // =============================================================
-// Create New Characters - takes in JSON input
 
-//return the friends array
-app.get("/api/friends", function(req, res){
-  res.json(reservations);
-});
+var friends = require("../data/friends.js");
 
-//adds incoming survey data
-app.post("/api/friends", function(req, res) {
-  var newFriend = req.body;
 
-  console.log(newcharacter);
-  
-  friends.push(newFriend);
-});
+module.exports = function(app) {
+  //return the friends array
+  app.get("/api/friends", function(req, res){
+    res.json(reservations);
+  });
 
+  //adds incoming survey data
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+
+    console.log(newcharacter);
+    
+    friends.push(newFriend);
+  });
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,17 @@
+//Dependencies
+var path = require("path");
+
 // Routes
-// =============================================================
+var friends = require("../data/friends.js");
 
-// Basic route that sends the user first to the AJAX Page
-app.use("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
+module.exports = function(app) {
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-});
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+    // If no matching route is found default to home
+    app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
